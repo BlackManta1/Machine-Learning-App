965eec226500ce2e760c77a018b4b3685f42c951
Saliou DJALO <djalosaliou@gmail.com>
2019-07-07 14:05:20 +0400

    Initial Commit

---
+++ CoreML App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML App
+//
+//  Created by Saliou DJALO on 07/07/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
